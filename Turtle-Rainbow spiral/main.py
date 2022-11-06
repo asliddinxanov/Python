8941ca4bc3673e2a2378c8d9c61b2bc1d0fd567e
Asliddin Boburkhanov <100646459+asliddinxanov@users.noreply.github.com>
2022-11-06 10:06:18 +0900

    update

--- Turtle-Rainbow spiral/main.py
+++ Turtle-Rainbow spiral/main.py
@@ -1,25 +1,25 @@
 import turtle
 from turtle import *
 turtle.setup(1000, 1000, 0, 0)
-turtle.title('rainbow spiral')
 turtle.bgcolor('black')
 turtle.speed(0)
+
 r, g, b = 255, 0, 0
 
 for i in range(255*2):
     colormode(255)
     if i < 255//3:
-        g += 3
+        g += 2
     elif i < 255*2//3:
-        r -= 3
+        r -= 2
     elif i < 255:
-        b += 3
+        b += 2
     elif i < 255*4//3:
-        g -= 3
+        g -= 2
     elif i < 255*5//3:
-        r += 3
+        r += 2
     else:
-        b -= 3
+        b -= 20
     fd(50+i)
-    rt(91)
+    rt(111)
     pencolor(r, g, b)
